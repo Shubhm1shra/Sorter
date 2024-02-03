b525517c1f4a04527ea3e8894c0c3c141754ca0f
Shreshth Mishra <92676871+Shubhm1shra@users.noreply.github.com>
2024-02-03 23:04:29 +0530

    Added Controlled ArraySize handling

--- Sorter.cpp
+++ Sorter.cpp
@@ -3,6 +3,12 @@
 
 #include "Sorter.hpp"
 
+/*
+Parameterised Construction :-
+sf::RenderWindow& window -> Reference to predefined window
+int arrSize -> Size of Array to Sort
+const std::string& algo -> Algo Name
+*/
 Sorter::Sorter(sf::RenderWindow& window, int arrSize, const std::string& algo) : m_window(window), arraySize(arrSize){
     initialize();
 
@@ -16,22 +22,45 @@ Sorter::Sorter(sf::RenderWindow& window, int arrSize, const std::string& algo) :
     }
 }
 
+// No Allocations in heap -> No need for removing anything (I think)
 Sorter::~Sorter() = default;
 
 void Sorter::start(){
-    
+    sf::Event event;
+
+    int step_itr = 0;
+    std::size_t total_steps = steps.size();
+
+    while(m_window.isOpen()){
+        while(m_window.pollEvent(event)){
+            if(event.type == sf::Event::Closed) m_window.close();
+            if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) m_window.close();
+            if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::R) step_itr = 0;
+            if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Left) step_itr -= 10;
+            if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Right) step_itr += 10;
+        }
+
+        if(step_itr < 0) step_itr = 0;
+        if(step_itr > total_steps - 1) step_itr = total_steps - 1;
+
+        drawArray(steps[step_itr]);
+        step_itr++;
+    }
 }
 
+// Helper function for Testing
 void showVector(std::vector<int> arr){
     for(int i : arr) std::cout << i << " ";
     std::cout << std::endl;
 }
 
+// Helper function for Testing
 void showVector(std::vector<float> arr){
     for(int i : arr) std::cout << i << " ";
     std::cout << std::endl;
 }
 
+// Helper function for Testing
 void showMatrix(std::vector<std::vector<int>> arr){
     for(std::vector<int> row : arr){
         for(int i : row) std::cout << i << " ";
@@ -40,24 +69,31 @@ void showMatrix(std::vector<std::vector<int>> arr){
     std::cout << std::endl;
 }
 
+// Just for changing code here rather than start function
 void Sorter::test(){
     sf::Event event;
 
-    int count = 0;
+    int step_iter = 0;
     std::size_t total_steps = steps.size();
 
     while(m_window.isOpen()){
         while(m_window.pollEvent(event)){
             if(event.type == sf::Event::Closed) m_window.close();
             if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) m_window.close();
+            if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space) step_iter = 0;
         }
 
-        drawArray(steps[count]);
+        drawArray(steps[step_iter]);
 
-        if(count != total_steps - 1) count++;
+        if(step_iter != total_steps - 1) step_iter++;
     }
 }
 
+/*
+const std::vector<int>& arr -> Array to Sort
+
+Generates steps in form of std::vector<std::vector<int>> by sorting how bubble sort is supposed to, and pushing the updated vector to steps
+*/
 std::vector<std::vector<int>> Sorter::getBubbleSteps(const std::vector<int>& arr){
     std::vector<std::vector<int>> m_steps;
     std::vector<int> m_arr(arr);
@@ -79,6 +115,7 @@ std::vector<std::vector<int>> Sorter::getBubbleSteps(const std::vector<int>& arr
     return m_steps;
 }
 
+// Same stuff as Bubble Sort as Selection Sort
 std::vector<std::vector<int>> Sorter::getSelectionSteps(const std::vector<int>& arr){
     std::vector<std::vector<int>> m_steps;
     std::vector<int> m_arr(arr);
@@ -140,6 +177,7 @@ void Sorter::merge(std::vector<std::vector<int>>& m_steps, std::vector<int>& arr
     }
 }
 
+// Same stuff as Bubble Sort but Merge
 std::vector<std::vector<int>> Sorter::getMergeSteps(const std::vector<int>& arr){
     std::vector<std::vector<int>> m_steps;
     std::vector<int> m_arr(arr);
@@ -175,6 +213,7 @@ int Sorter::partition(std::vector<std::vector<int>>& m_steps, std::vector<int>& 
     return (i+1);
 }
 
+// Same stuff as Bubble Sort but Quick Sort
 std::vector<std::vector<int>> Sorter::getQuickSteps(const std::vector<int>& arr){
     std::vector<std::vector<int>> m_steps;
     std::vector<int> m_arr(arr);
@@ -208,6 +247,7 @@ std::vector<std::vector<int>> Sorter::getQuickSteps(const std::vector<int>& arr)
     return m_steps;
 }
 
+// Take the Vector -> Normalize the vector -> Scale it to Scaling Factor -> Calculate Height for individual data (width calcualted on the basis of vector size) -> Create sf::RectagleShape and draw on window
 void Sorter::drawArray(const std::vector<int>& arr){
     m_window.clear();
 
@@ -215,7 +255,7 @@ void Sorter::drawArray(const std::vector<int>& arr){
 
 
     for(int i = 0; i < arraySize; i++){
-        sf::RectangleShape rect(sf::Vector2f(rectWidth - 1, normalHeights[i] * SCALE_FACTOR));
+        sf::RectangleShape rect(sf::Vector2f(rectWidth, normalHeights[i] * SCALE_FACTOR));
         rect.setPosition((rectWidth + PADDINGX) * i, m_window.getSize().y - normalHeights[i] * SCALE_FACTOR - PADDINGY);
 
         m_window.draw(rect);
@@ -243,12 +283,19 @@ std::vector<float> Sorter::getNormalizedHeight(const std::vector<int>& arr){
     return result;
 }
 
+// Set seed on the basis of time (to get better random values) -> Assign Random value in Data Vector -> Calculate Width for each Data Value
 void Sorter::initialize(){
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
     setRandArray();
 
-    rectWidth = static_cast<float>(m_window.getSize().x / arraySize) - PADDINGX;
+    if(arraySize > m_window.getSize().x) arraySize = m_window.getSize().x;
+    rectWidth = static_cast<float>(m_window.getSize().x) / static_cast<float>(arraySize) - PADDINGX;
+
+    if(rectWidth <= 0){
+        arraySize = static_cast<int>(m_window.getSize().x) / (PADDINGX + 1) ;
+        rectWidth = static_cast<float>(m_window.getSize().x) / static_cast<float>(arraySize) - PADDINGX;
+    }
 
     maxInArr = dataArray[0];
     minInArr = 1;
