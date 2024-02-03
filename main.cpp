13ffec20f9e39303a34b447c96960e29340497c9
Shreshth Mishra <92676871+Shubhm1shra@users.noreply.github.com>
2024-02-03 23:05:22 +0530

    Taking Algorithm at Run-Time

--- main.cpp
+++ main.cpp
@@ -1,15 +1,37 @@
 #include "Sorter.hpp"
 
+#include <iostream>
+#include <conio.h>
+
 const int nScreenWidth = 800;
 const int nScreenHeight = 600;
 
 int main(void){
+    std::string algo = "";
+    std::cout << "Algorithm : ";
+    std::cin >> algo;
+
+    if(algo.size() == 0 || algo == "b" || algo == "Bubble Sort") algo = "Bubble Sort";
+    else if(algo == "s" || algo == "Selection Sort")  algo = "Selection Sort";
+    else if(algo == "m" || algo == "Merge Sort") algo = "Merge Sort";
+    else if(algo == "q" || algo == "Quick Sort") algo = "Quick Sort";
+    else{
+        std::cerr << "Invalid Algorighm Name provided!" << std::endl;
+        _getch();
+        return 1;
+    }
+
+    std::cout << std::endl;
+
     sf::RenderWindow window(sf::VideoMode(nScreenWidth, nScreenHeight), "Sorter!");
     window.setVerticalSyncEnabled(true);
+    std::cout << window.getSize().x << std::endl;
 
-    Sorter sort(window, 400, "Merge Sort");
+    window.requestFocus();
 
-    sort.test();
+    Sorter sort(window, 900, algo);
+
+    sort.start();
 
     return 0;
 }
